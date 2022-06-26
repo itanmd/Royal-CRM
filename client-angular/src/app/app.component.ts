72a28c284c1835126ada7fb82f822b3cf88785cf
Itan <itanmoshe@gmail.com>
2022-06-26 20:53:16 +0300

    signup + login pages

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'welcome';
-  inputType = 'text';
-  success= true;
-  night = true;
-  items = ['orange', 'apple', 'banana'];
-  labels = ['Customers', 'Products', 'Orders'];
 
-  
-  toggleTitleColor() {
-    this.success = !this.success;
-  }
-  isItNight() {
-    return this.night;
-  }
-  addAnotherItem() {
-    this.items.push('papaya');
-  }
 }
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
@@ -7,28 +9,30 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CustomersComponent } from './customers/customers.component';
-import { AppRoutingModule } from './app-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { OrdersComponent } from './orders/orders.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    CustomersComponent,
-    ProductsComponent,
-    OrdersComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ButtonComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        CustomersComponent,
+        ProductsComponent,
+        OrdersComponent,
+        SignupComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
