eab71960aea2d95077456e718529111ad7e2b6e0
Itan <itanmoshe@gmail.com>
2022-06-26 21:10:48 +0300

    services

---
+++ client-angular/src/app/services/session.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+
+  constructor(private router: Router) { 
+    
+  }
+}
